266d3320d480db2cab15f6c43fa01a17dd8caa2a
dbaldassare99 <120267926+dbaldassare99@users.noreply.github.com>
2024-02-02 16:35:58 -0800

    Update submit_job.py

--- submit_job.py
+++ submit_job.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-import make_slurm
         
 job_file = #Job File
 account = #Account
@@ -27,5 +26,23 @@ if ipynb == True: #turn Jupyter notebook into python file
     pyfile = ipynbf[:-5]+'py'
     
 subprocess.run(['chmod','755',pyfile]) #Makes file executable
+
+ith open(job_file,'w') as fh: #create slurm batch script
+    fh.writelines('#!/bin/csh\n')
+    fh.writelines('#SBATCH --time='+time+'\n')
+    fh.writelines('#SBATCH -o slurm-%j.out-%N'+'\n')
+    fh.writelines('#SBATCH -e slurm-%j.err-%N'+'\n')
+    fh.writelines('#SBATCH --nodes='+str(nnodes)+'\n')
+    fh.writelines('#SBATCH --ntasks='+str(ntasks)+'\n')
+    fh.writelines('#SBATCH --account='+account+'\n')
+    fh.writelines('#SBATCH --partition='+partition+'\n')
+    fh.writelines('#SBATCH -M'+cluster+'\n')
+    if mem is not None:
+        fh.writelines('#SBATCH --mem='+mem+'\n')
+    fh.writelines('setenv WORKDIR '+wd+'\n')
+    fh.writelines(module+'\n')
+    fh.writelines(moduleload+'\n')
+    fh.writelines(conda+'\n')
+    fh.writelines('python ' + pyfile)
 
 subprocess.run(["sbatch","--export=NONE",job_file]) #Submit python file via slurm batch script
