658e1e5fb7198409d94c1e237efc593f3ea51f11
dbaldassare99 <120267926+dbaldassare99@users.noreply.github.com>
2024-02-02 16:33:00 -0800

    Update submit_job.py

--- submit_job.py
+++ submit_job.py
@@ -1,10 +1,5 @@
 import os
 import subprocess
-
-def mkfile(file):
-    '''make a directory (dir) if it doesn't exist'''
-    if not os.path.exists(dir):
-        os.mkfile(file)
         
 job_file = #Job File
 account = #Account
@@ -15,8 +10,8 @@ time = #Time
 nnodes = #Number of Nodes
 ntasks = #Number of Tasks
 wd = '$HOME'
-module = 'module use $HOME/MyModules/'
-moduleload = 'module load miniconda3/latest'
+module = 'module use $HOME/MyModules/' 
+moduleload = 'module load miniconda3/latest' #Can replace with different module loading
 conda = 'conda activate ' + #Environment Name
 ipynb = False
 ipynbf = 'cds_cmip6_download.ipynb'
@@ -26,28 +21,10 @@ if ipynbf[-6:-1] == 'ipynb':
 else:
     pyfile = ipynbf
     
-if ipynb == True:
+if ipynb == True: #turn Jupyter notebook into python file
     subprocess.run(['jupyter','nbconvert', '--to','script', ipynbf])
     pyfile = ipynbf[:-5]+'py'
     
-subprocess.run(['chmod','755',pyfile])
+subprocess.run(['chmod','755',pyfile]) #Makes file executable
 
-with open(job_file,'w') as fh:
-    fh.writelines('#!/bin/csh\n')
-    fh.writelines('#SBATCH --time='+time+'\n')
-    fh.writelines('#SBATCH -o slurm-%j.out-%N'+'\n')
-    fh.writelines('#SBATCH -e slurm-%j.err-%N'+'\n')
-    fh.writelines('#SBATCH --nodes='+str(nnodes)+'\n')
-    fh.writelines('#SBATCH --ntasks='+str(ntasks)+'\n')
-    fh.writelines('#SBATCH --account='+account+'\n')
-    fh.writelines('#SBATCH --partition='+partition+'\n')
-    fh.writelines('#SBATCH -M'+cluster+'\n')
-    if mem is not None:
-        fh.writelines('#SBATCH --mem='+mem+'\n')
-    fh.writelines('setenv WORKDIR '+wd+'\n')
-    fh.writelines(module+'\n')
-    fh.writelines(moduleload+'\n')
-    fh.writelines(conda+'\n')
-    fh.writelines('python ' + pyfile)
-
-subprocess.run(["sbatch","--export=NONE",job_file])
+subprocess.run(["sbatch","--export=NONE",job_file]) #Submit python file via slurm batch script
